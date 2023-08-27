1d597b17c1da3db71160da97ce7af63f3d44af72
Arn.28 <arnold.balabarca.27@unsch.edu.pe>
2023-08-27 00:42:39 -0500

    Updates

--- src/components/PageContent.tsx
+++
@@ -1,7 +0,0 @@
-import './PageContent.scss'
-interface IProps {
-  children?: React.ReactNode
-}
-export const PageContent = ({ children }: IProps) => (
-  <div className='page-content__container'>{children}</div>
-)
